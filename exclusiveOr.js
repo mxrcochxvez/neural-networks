de615b2c4dad2c6c2521f8bcf2b3437554e1c790
Marco Chavez <marcode.chavez.jr@gmail.com>
2025-09-07 22:50:39 -0700

    feat(brain): adds brain.js to the project for intellisense

--- exclusiveOr.js
+++ exclusiveOr.js
@@ -1,1 +1,16 @@
-console.log('test');
+import brain from 'brain.js';
+
+const network = new brain.NeuralNetwork({ hiddenLayers: [3] });
+
+const trainingData = [
+	{ input: [0, 0], output: [0] },
+	{ input: [0, 1], output: [1] },
+	{ input: [1, 0], output: [1] },
+	{ input: [1, 1], output: [0] },
+]
+
+network.train(trainingData);
+
+const prediction = Math.round(network.run([0, 0]));
+
+console.log(prediction);

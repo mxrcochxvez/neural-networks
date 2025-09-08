8ed65b47e9394ee254a2ffa6eb490229881c66a0
Marco Chavez <marcode.chavez.jr@gmail.com>
2025-09-08 00:21:11 -0700

    feat(package.json): adds routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { predict } from './exclusiveOr';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import XorDemo from './pages/XORDemo';
 
 export default function App() {
   return (
-    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
-      <h1>Neural Network XOR Demo</h1>
-      <p>0 XOR 0 = {predict(0, 0)}</p>
-      <p>0 XOR 1 = {predict(0, 1)}</p>
-      <p>1 XOR 0 = {predict(1, 0)}</p>
-      <p>1 XOR 1 = {predict(1, 1)}</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/xor" element={<XorDemo />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
